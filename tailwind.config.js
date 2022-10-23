eb30b020ce821c7a882062a6a56d443e4adef826
Whyareyou_ <54592995+Whyareyouu@users.noreply.github.com>
2022-10-23 17:59:31 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.html"],
+	theme: {
+		screens: {
+			lg: { max: "992px" },
+			md: { max: "768px" },
+			sm: { max: "480px" },
+		},
+		container: {
+			padding: "20px",
+			center: true,
+		},
+		extend: {
+			colors: {
+				lightgray: "#d1d1d1",
+				lightwhite: "#f7f4f1",
+			},
+		},
+	},
+	plugins: [],
+};
